fa7bf0fe67770b04ac6dc710554432714b868ee8
Seth Cram <sethcram@gmail.com>
2023-05-03 17:16:00 -0700

    programmatically run gunicorn

--- code/api.py
+++ code/api.py
@@ -5,6 +5,7 @@ import shutil
 from uvicorn import run
 from fastapi.middleware.cors import CORSMiddleware
 from sqlite3 import Connection, connect, OperationalError
+import gunicorn.app.base
 
 #region Helper Functs
         
@@ -238,7 +239,8 @@ def getDatabases():
     try:
         #get all names in db folder
         db_folders = os.listdir("database")
-    except:
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail="There was an error when attempting to list all the database folders.")
     
     #take only the directories within the db folder names
@@ -289,5 +291,32 @@ def deleteSqlDb(file_name: str):
     
     return {"message": correct_msg}
 
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+    
+# Use a debugging session in port 5001    
+options = {
+    'bind': '%s:%s' % ('0.0.0.0', '8000'),
+    'workers': 1,#number_of_workers(),
+    # 'threads': number_of_workers(),
+    'timeout': 120,
+    'forwarded_allow_ips': '*',
+    "worker_class": 'uvicorn.workers.UvicornWorker'
+}
+StandaloneApplication(app, options).run()
+
 # Run app
-run(app=app, host="0.0.0.0", port=8000, forwarded_allow_ips='*')
+#run(app=app, host="0.0.0.0", port=8000, forwarded_allow_ips='*')
